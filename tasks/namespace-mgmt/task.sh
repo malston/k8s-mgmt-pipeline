38b076ad73066018dbcff0885e4a06499d067ab7
Mark Alston <malston@pivotal.io>
2020-04-14 18:00:25 -0600

    Add pks and kube configs as inputs

--- tasks/namespace-mgmt/task.sh
+++ tasks/namespace-mgmt/task.sh
@@ -127,4 +127,11 @@ if [[ -z "${password}" ]]; then
   exit 1
 fi
 
+mkdir -p ~/.pks
+cp pks-config/creds.yml ~/.pks/creds.yml
+
+mkdir -p ~/.kube
+cp kube-config/config ~/.kube/config
+
+
 main "$password" "$delete_flag"
